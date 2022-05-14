8a3a51a9b782cd27a9bad6ca88238046a69f596d
Manuel Selbach <manuel_selbach@yahoo.de>
2022-05-14 15:50:59 +0200

    Add test for MigrateLoginRequiredAnnotationToRouteRector

---
+++ tests/Rector/v65/MigrateLoginRequiredAnnotationToRouteRector/MigrateLoginRequiredAnnotationToRouteRectorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frosh\Rector\Tests\Rector\v65\MigrateLoginRequiredAnnotationToRouteRector;
+
+use Frosh\Rector\Tests\Rector\AbstractFroshRectorTestCase;
+
+final class MigrateLoginRequiredAnnotationToRouteRectorTest extends AbstractFroshRectorTestCase
+{
+}
