0938f1308bbd67797cc3d1ccce50b81424a0f69d
Soner Sayakci <s.sayakci@shopware.com>
2022-06-09 11:23:45 +0200

    feat: ContextMetadataExtensionToStateRector fixes #7

---
+++ src/Rule/v65/ContextMetadataExtensionToStateRector.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frosh\Rector\Rule\v65;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Type\ObjectType;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+class ContextMetadataExtensionToStateRector extends AbstractRector
+{
+    private const ALLOWED_CONSTS = ['USE_INDEXING_QUEUE', 'DISABLE_INDEXING'];
+
+    public function getRuleDefinition(): \Symplify\RuleDocGenerator\ValueObject\RuleDefinition
+    {
+        return new RuleDefinition(
+            'Migrate extension metadata to state rector',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+$context->addExtension(EntityIndexerRegistry::USE_INDEXING_QUEUE, new ArrayEntity());
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+$context->addState(EntityIndexerRegistry::USE_INDEXING_QUEUE);
+CODE_SAMPLE
+                ),
+            ],
+        );
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [
+            MethodCall::class,
+        ];
+    }
+
+    /**
+     * @param MethodCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (!$this->isObjectType($node->var, new ObjectType('Shopware\Core\Framework\Context'))) {
+            return null;
+        }
+
+        if ((string) $node->name !== 'addExtension') {
+            return null;
+        }
+
+        /** @var Node|Node\Expr\ClassConstFetch $arg1 */
+        $arg1 = $node->args[0]->value;
+
+        if (!$arg1  instanceof Node\Expr\ClassConstFetch || !\in_array($arg1->name->toString(), self::ALLOWED_CONSTS, true)) {
+            return null;
+        }
+
+        return new MethodCall($node->var, 'addState', [$node->args[0]]);
+    }
+}
---
+++ tests/Rector/v65/ContextMetadataExtensionToStateRector/config/configured_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Frosh\Rector\Rule\v65\ContextMetadataExtensionToStateRector;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config_test.php');
+    $rectorConfig->rule(ContextMetadataExtensionToStateRector::class);
+};
