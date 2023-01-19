033b2559ca4e26a7701916ff99d5ac2e92d228e0
Soner Sayakci <s.sayakci@shopware.com>
2023-01-19 16:31:25 +0100

    Enabled symfony rules by default

--- config/shopware-6.5.0.php
+++ config/shopware-6.5.0.php
@@ -3,7 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/v6.5/*');
+    $rectorConfig->sets([
+        SymfonySetList::SYMFONY_54,
+        SymfonySetList::SYMFONY_60,
+        SymfonySetList::SYMFONY_61,
+        SymfonySetList::SYMFONY_62,
+    ]);
 };
