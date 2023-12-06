11f7d20f6e283370040685e65706af8d3ee5b33f
max <max@swk-web.com>
2023-12-06 15:56:36 +0100

    fix: Use explict path to rector config

--- config/shopware-6.6.0.php
+++ config/shopware-6.6.0.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/v6.6/*');
+    $rectorConfig->import(__DIR__ . '/v6.6/renaming.php');
 
     $rectorConfig->importNames();
     $rectorConfig->importShortClasses(false);
