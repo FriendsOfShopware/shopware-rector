16515fdb3c2e65636ba1e120724e900c72f3c4ee
Max <aragon999@users.noreply.github.com>
2024-01-27 16:01:16 +0100

    feat: Add PHP and Symfony sets to Shopware 6.6 (#30)

--- config/shopware-6.6.0.php
+++ config/shopware-6.6.0.php
@@ -3,10 +3,18 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/v6.6/renaming.php');
     $rectorConfig->import(__DIR__ . '/v6.6/exceptions.php');
+
+    $rectorConfig->sets([
+        SymfonySetList::SYMFONY_63,
+        SymfonySetList::SYMFONY_64,
+        LevelSetList::UP_TO_PHP_82,
+    ]);
 
     $rectorConfig->importNames();
     $rectorConfig->importShortClasses(false);
