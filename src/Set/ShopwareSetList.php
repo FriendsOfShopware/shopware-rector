e061e02a2cd7ed21e81449d7fb7ae5199136ce64
Max <aragon999@users.noreply.github.com>
2024-05-27 18:25:33 +0200

    Feat/deprecated constants (#35)
    * feat: Add renaming for deprecated CartEvents constant
    
    * feat: Add MailSendSubscriberConfig constant deprecations for Shopware 6.7

--- src/Set/ShopwareSetList.php
+++ src/Set/ShopwareSetList.php
@@ -15,4 +15,9 @@ final class ShopwareSetList
      * @var string
      */
     public const SHOPWARE_6_6_0 = __DIR__ . '/../../config/shopware-6.6.0.php';
+
+    /**
+     * @var string
+     */
+    public const SHOPWARE_6_7_0 = __DIR__ . '/../../config/shopware-6.7.0.php';
 }
