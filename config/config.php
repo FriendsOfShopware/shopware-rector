f1ced44a24c7ede330fd165fea010def3d772f88
Joshua Behrens <behrens@heptacom.de>
2025-04-15 05:52:39 +0200

    Skip vendor folders to not try apply rector on vendor folders next to source code files

--- config/config.php
+++ config/config.php
@@ -5,4 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->skip([
+        '*/vendor/*',
+    ]);
 };
