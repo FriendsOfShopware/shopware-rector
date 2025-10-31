f81071d3bab3099775c5a7253e13ad069a4cabcb
max <max@swk-web.com>
2025-10-31 20:27:10 +0100

    refactor: Remove not needed variable doctypes of constants

--- src/Set/ShopwareSetList.php
+++ src/Set/ShopwareSetList.php
@@ -6,28 +6,13 @@ namespace Frosh\Rector\Set;
 
 final class ShopwareSetList
 {
-    /**
-     * @var string
-     */
     public const SHOPWARE_6_5_0 = __DIR__ . '/../../config/shopware-6.5.0.php';
 
-    /**
-     * @var string
-     */
     public const SHOPWARE_6_6_0 = __DIR__ . '/../../config/shopware-6.6.0.php';
 
-    /**
-     * @var string
-     */
     public const SHOPWARE_6_6_4 = __DIR__ . '/../../config/shopware-6.6.4.php';
 
-    /**
-     * @var string
-     */
     public const SHOPWARE_6_6_10 = __DIR__ . '/../../config/shopware-6.6.10.php';
 
-    /**
-     * @var string
-     */
     public const SHOPWARE_6_7_0 = __DIR__ . '/../../config/shopware-6.7.0.php';
 }
